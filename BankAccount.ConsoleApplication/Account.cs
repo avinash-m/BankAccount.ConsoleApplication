1dacd6013334fd69032b2c188333d360c5f5425b
Avinash M <avinashm201993@gmail.com>
2018-08-16 11:26:29 -0500

    Implemented withdraw functionality

--- BankAccount.ConsoleApplication/Account.cs
+++ BankAccount.ConsoleApplication/Account.cs
@@ -7,7 +7,7 @@ namespace BankAccount.ConsoleApplication
         public string Name { get; set; }
         public int Age { get; set; }
         public string AccountNumber { get; set; }
-        public int Amount { get; set; }
+        public int Balance { get; set; }
 
         public void GetDetails()
         {
@@ -16,10 +16,18 @@ namespace BankAccount.ConsoleApplication
             Console.Write("Enter age: ");
             Age = Convert.ToInt16(Console.ReadLine());
             Console.Write("Enter account number: ");
-            AccountNumber = Console.ReadLine();            
+            AccountNumber = Console.ReadLine();
+            Console.Write("Enter amount to deposit: ");
+            Balance = Convert.ToDouble(Console.ReadLine());
         }
-                
 
-        
+        public void Withdraw(double withdrawAmount)
+        {
+            if (withdrawAmount <= Amount)
+                Balance -= withdrawAmount;
+            else
+                Console.WriteLine("Withdraw amount is more than the available balance.");
+        }
+
     }
 }
