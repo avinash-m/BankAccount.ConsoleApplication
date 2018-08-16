d95a60bd9bbc4643123a71f7e34bc9f06e72dbf8
Avinash M <avinashm201993@gmail.com>
2018-08-16 11:18:44 -0500

    Implemented function to get details

---
+++ BankAccount.ConsoleApplication/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BankAccount.ConsoleApplication
+{
+    class Account
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string AccountNumber { get; set; }
+        public int Amount { get; set; }
+
+        public void GetDetails()
+        {
+            Console.Write("Enter name: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter age: ");
+            Age = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter account number: ");
+            AccountNumber = Console.ReadLine();            
+        }
+                
+
+        
+    }
+}
